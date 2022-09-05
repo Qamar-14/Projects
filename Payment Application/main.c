2648b04d45a40b74822462f697dcb61ada80fdbf
Qamar-14 <104042729+Qamar-14@users.noreply.github.com>
2022-09-05 22:42:50 +0200

    Add files via upload

---
+++ Payment Application/main.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include"app.h"
+
+
+#include <time.h>
+#include <stdio.h>
+#include "stdint.h"
+
+int main() {
+	appStart();
+	return 0;
+}
